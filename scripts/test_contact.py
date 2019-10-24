3425baff6975bcc0fe0ae569c96089c8c3292da3
zhs766575 <zhs766575@gmail.com>
2019-10-24 10:55:13 +0800

    haha

---
+++ scripts/test_contact.py
@@ -0,0 +1,16 @@
+import time
+
+import allure
+import pytest
+from appium import webdriver
+
+class TestContact:
+
+    def test_contact1(self):
+        assert True
+
+    def test_contact2(self):
+        assert False
+
+    def test_contact3(self):
+        assert True
